e0c601ff4b0ce8e627b11cc58d0349e02620ab26
hoangkimduc <duc.hoang@employmenthero.com>
2022-05-22 17:59:07 +0700

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001"
+    origins "http://localhost:3001", "http://127.0.0.1:3001", "http://127.0.0.1:3000", "http://localhost:3000"
 
     resource "*",
       headers: :any,
