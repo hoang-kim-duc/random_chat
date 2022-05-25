c0368606a35fd93812c13577895def15d441d973
hoangkimduc <duc.hoang@employmenthero.com>
2022-05-25 23:04:35 +0700

    send mails to team

---
+++ config/initializers/staging_mailer_interceptor.rb
@@ -0,0 +1,6 @@
+class StagingMailerInterceptor
+  def self.delivering_email(message)
+    message.to = ENV['EMAIL_RECIPIENT'].split
+  end
+end
+ActionMailer::Base.register_interceptor(StagingMailerInterceptor) if Rails.env.staging?
