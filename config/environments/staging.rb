2e812a1b0988c0164edf06031a3d71b6febd3f14
hoangkimduc <duc.hoang@employmenthero.com>
2022-06-16 22:11:50 +0700

    fix cor for socket

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -101,6 +101,6 @@ Rails.application.configure do
   }
 
   config.action_cable.allowed_request_origins = ['http://localhost:3001', 'http://127.0.0.1:3001',
-    'http://localhost:3000', 'http://127.0.0.1:3000']
+    'http://localhost:3000', 'http://127.0.0.1:3000', ENV['FE_HOST']]
 
 end
