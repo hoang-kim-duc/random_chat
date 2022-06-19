1fa8433d89c54575487f239086abe51d6ac50339
hoangkimduc <duc.hoang@employmenthero.com>
2022-06-19 18:21:24 +0700

    add cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3001", "http://127.0.0.1:3001", "http://127.0.0.1:3000", "http://localhost:3000",
-      ENV['FE_HOST']
+      ENV['FE_HOST'], 'http://sub.cherishbychating.com'
 
     resource "*",
       headers: :any,
