fb11a5fe64879de9823081e6b00fffbd85eb7b80
hoangkimduc <duc.hoang@employmenthero.com>
2022-07-02 16:43:59 +0700

    fix messages serializer

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -2,6 +2,6 @@ class ApplicationSerializer < ActiveModel::Serializer
   def process_time(datetime)
     return nil unless datetime
 
-    datetime.in_time_zone(object.recipient.time_zone).strftime('%H:%M %d/%m/%Y')
+    datetime.in_time_zone(object.recipient.time_zone).strftime('%Y-%m-%dT%H:%M:%S.%L%z')
   end
 end
