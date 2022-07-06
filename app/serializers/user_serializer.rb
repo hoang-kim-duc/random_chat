14be9a39dd1f8026b514ed568ec806c56c54b24f
hoangkimduc <duc.hoang@employmenthero.com>
2022-07-06 22:18:15 +0700

    add jwt token to user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ApplicationSerializer
-  attributes :id, :name, :avatar_url, :last_online
+  attributes :id, :name, :avatar_url, :last_online, :jwt_token
   attribute :online?, key: :is_online
 end
