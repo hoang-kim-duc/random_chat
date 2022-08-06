24efe7bf9587f92809840dcfd64cf0b9024c2816
hoangkimduc <duc.hoang@employmenthero.com>
2022-08-06 12:00:55 +0700

    add allow host production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << ENV['FE_HOST']
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: ENV['FE_HOST'] || 'localhost:3000' }
 
