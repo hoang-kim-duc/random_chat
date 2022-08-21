1cf5bae0a8549452d7e47a5f1be2e4997fe71e96
hoangkimduc <duc.hoang@employmenthero.com>
2022-08-22 00:51:16 +0700

    update received status

--- app/channels/new_message_channel.rb
+++ app/channels/new_message_channel.rb
@@ -2,6 +2,7 @@
 
 class NewMessageChannel < ApplicationCable::Channel
   def subscribed
+    current_user.received_messages.sent.each(&:broadcast!)
     stream_from Broadcaster::URL.new_message(current_user.id)
   end
 end
